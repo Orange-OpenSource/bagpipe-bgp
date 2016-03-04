f07e05c2ee3d475be31bcca67a242c078a3de548
Thomas Morin <thomas.morin@orange.com>
2016-03-04 10:09:12 +0100

    Avoid "ifconfig|grep HWaddr" to find the MAC of a device
    It is best to not depend on ifconfig, which output is not always the same
    in different releases.
    
    This rewrite uses /sys/class/net/<if>/address to find the MAC of a device,
    in the different place where ifconfig was used.
    
    The code is also factored out when possible.
    
    (+ some minor cleanups)

---
+++ bagpipe/bgp/common/net_utils.py
@@ -0,0 +1,32 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# encoding: utf-8
+
+# Copyright 2016 Orange
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def get_device_mac(run_command_fn, dev_name, netns_name=None):
+    """ Find device MAC address """
+    if netns_name:
+        command_prefix = "ip netns exec %s " % netns_name
+    else:
+        command_prefix = ""
+
+    (output, _) = run_command_fn("%scat /sys/class/net/%s/address" %
+                                 (command_prefix, dev_name))
+    return output[0]
