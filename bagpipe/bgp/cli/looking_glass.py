Orange-OpenSource/bagpipe-bgp

commit 66aa75ba58320b80563849797f95f88c19da1e2b

Looking glass improvements
- provide external_instance_id under "vpns instances"
- fix href to ipvpn from evpn gateway port
- looking glass client will follow hrefs at the root of the result
- no space between VPN instance and number: e.g. "EVI-5" instead of "EVI 5"

bagpipe/bgp/cli/looking_glass.py
@@ -145,6 +145,14 @@ e.g.: %prog vpns instances"""
 
         if response.getcode() == 200:
             data = json.load(response)
+
+            if ("href" in data):
+                target_url_bis = data["href"]
+                response_bis = urllib2.urlopen(target_url_bis)
+                if response.getcode() == 200:
+                    target_url = target_url_bis
+                    data = json.load(response_bis)
+
             pretty_print_recurse(data, 0, options.recurse, target_url,
                                  alreadyANewLine=True)
 

bagpipe/bgp/vpn/__init__.py
@@ -450,7 +450,9 @@ class VPNManager(LookingGlass):
         }
 
     def getLGVPNList(self):
-        return [{"id": i} for i in self.vpnInstances.iterkeys()]
+        return [{"id": id,
+                 "name": instance.name}
+                for (id, instance) in self.vpnInstances.iteritems()]
 
     def getLGVPNFromPathItem(self, pathItem):
         return self.vpnInstances[pathItem]

bagpipe/bgp/vpn/evpn/__init__.py
@@ -296,12 +296,17 @@ class EVI(VPNInstance, LookingGlass):
 
     def getLookingGlassLocalInfo(self, pathPrefix):
         if not self.gwPort:
-            return {"gwPort": None}
+            return {"gateway_port": None}
         else:
             (linuxif, ipvpn) = self.gwPort
-            return {"gwPort": {
+            return {"gateway_port": {
                     "interface": repr(linuxif),
-                    "ipvpn": LookingGlassReferences.getAbsolutePath(
-                        "VPN_INSTANCES", pathPrefix,
-                        [ipvpn.externalInstanceId]),
+                    "ipvpn": {"href":
+                              LookingGlassReferences.getAbsolutePath(
+                                  "VPN_INSTANCES", pathPrefix,
+                                  [ipvpn.externalInstanceId]),
+                              "id": ipvpn.name,
+                              "external_instance_id":
+                                  ipvpn.externalInstanceId
+                              },
                     }}

bagpipe/bgp/vpn/vpn_instance.py
@@ -261,7 +261,7 @@ class VPNInstance(TrackerWorker, Thread, LookingGlassLocalLogger):
         else:
             compareRoutes = compareNoECMP
 
-        TrackerWorker.__init__(self, self.vpnManager.bgpManager, "%s %d" %
+        TrackerWorker.__init__(self, self.vpnManager.bgpManager, "%s-%d" %
                                (self.instanceType, self.instanceId),
                                compareRoutes)
 
@@ -758,6 +758,7 @@ class VPNInstance(TrackerWorker, Thread, LookingGlassLocalLogger):
 
     def getLGMap(self):
         return {
+            "external_instance_id":   (LGMap.VALUE, self.externalInstanceId),
             "dataplane":     (LGMap.DELEGATE, self.dataplane),
             "route_targets": (LGMap.SUBITEM, self.getRTs),
             "gateway_ip":    (LGMap.VALUE, self.gatewayIP),

commit 5bef52f95955c27e5fa44616efcc929ff576fee6

Fix looking glass client
Fix a regression from 66aa75ba58320b80563849797f95f88c19da1e2b
that caused a looking glass request returning a string or array
to not be displayed.

bagpipe/bgp/cli/looking_glass.py
@@ -146,7 +146,7 @@ e.g.: %prog vpns instances"""
         if response.getcode() == 200:
             data = json.load(response)
 
-            if ("href" in data):
+            if (isinstance(data, dict) and "href" in data):
                 target_url_bis = data["href"]
                 response_bis = urllib2.urlopen(target_url_bis)
                 if response.getcode() == 200: