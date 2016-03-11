9475dfc63568f742a3e3b388a53e556ef82080b2
Thomas Morin <thomas.morin@orange.com>
2016-03-11 15:11:45 +0100

    impex2dot: support multiple servers and attract traffic
    bagpipe-impex2dot now supports --server multiple times
    to retrieve info from multiple servers and build one graph.
    
    For instance:
    bagpipe-impex2dot --server lnx01 --server lnx02 > graph.dot
    
    It also now supports the display of flow spec routes for
    attract traffic.

--- bagpipe/bgp/cli/impex2dot.py
+++ bagpipe/bgp/cli/impex2dot.py
@@ -8,62 +8,99 @@ BAGPIPE_PORT = 8082
 LOOKING_GLASS_BASE = "looking-glass"
 
 
-def request(options, args):
-    target_url = "http://%s:%d/%s/%s" % (options.server, options.port,
+def request(options, server, args):
+    target_url = "http://%s:%d/%s/%s" % (server, options.port,
                                          options.prefix, "/".join(args))
     try:
-        os.environ['NO_PROXY'] = options.server
+        os.environ['NO_PROXY'] = server
+        os.environ['no_proxy'] = server
         response = urllib2.urlopen(target_url)
 
         if response.getcode() == 200:
             return json.load(response)
+    except urllib2.HTTPError as e:
+        if e.code == 404:
+            return {}
+        print "Error requesting %s" % target_url
+        raise
     except:
+        print "Error requesting %s" % target_url
         raise
 
 
-def normalize(rtstring):
-    return (rtstring
+def normalize(string):
+    return (string
             .replace("target:", "rt:")
             .replace(":", "_")
             .replace("-", "_")
-            .replace("64512_", ""))
+            .replace(".", "_"))
 
-
-RT_STYLE = 'color="orange",textcolor="orange"'
+RT_TXT_STYLE = 'color="orange",fontcolor="orange"'
+RT_STYLE = 'color="orange",fontcolor="orange",arrowhead=onormal'
+RT_STYLE_REDIR_FROM_RT = ('color="orange",fontcolor="orange",style=dashed,'
+                          'arrowhead=diamond,arrowtail=oinv,headclip=false,'
+                          'dir=both')
+RT_STYLE_REDIR_TO_RT = ('color="orange",fontcolor="orange",style=dashed,'
+                        'arrowhead=onormal,tailclip=false,dir=both,'
+                        'arrowtail=oinv')
+RT_STYLE_FLOWSPEC = 'color="red",fontcolor="red",style=dotted'
+RT_STYLE_FLOWSPEC_INTER = ('color="red",fontcolor="red",style=dotted,'
+                           'arrowhead=none,dir=both,arrowtail=oinv')
+RT_STYLE_FLOWSPEC_ACTION = 'color="red",fontcolor="red",style=dashed'
+PORT_LINK_STYLE = 'style=dashed,dir=none,color="gray",weight=3'
 
 
 def get_all(options):
     rts = set()
-    vrfs = set()
+    vpns = set()
 
-    for vrf in request(options, ["vpns", "instances"]):
-        vrf_id = normalize(vrf['id'])
-        vrfs.add(vrf_id)
-        for rt_i in request(options, ["vpns", "instances", vrf_id,
-                                      "route_targets", "import"]):
-            rts.add(normalize(rt_i))
-        for rt_e in request(options, ["vpns", "instances", vrf_id,
-                                      "route_targets", "export"]):
-            rts.add(normalize(rt_e))
-        try:
-            readvertise = request(options, ["vpns", "instances", vrf_id,
-                                            "readvertise"])
-            if len(readvertise):
+    for server in options.servers:
+        for vpn in request(options, server, ["vpns", "instances"]):
+            vpn_id = vpn['id']
+            type = vpn['name'][:3]
+            vpns.add((server, vpn_id, type))
+            for rt_i in request(options, server, ["vpns", "instances", vpn_id,
+                                                  "route_targets", "import"]):
+                rts.add(normalize(rt_i))
+            for rt_e in request(options, server, ["vpns", "instances", vpn_id,
+                                                  "route_targets", "export"]):
+                rts.add(normalize(rt_e))
+
+            readvertise = request(options, server, ["vpns", "instances",
+                                                    vpn_id, "readvertise"])
+            if readvertise:
                 rts.add(normalize(readvertise['to'][0]))
                 rts.add(normalize(readvertise['from'][0]))
-        except urllib2.HTTPError as e:
-            if e.code != 404:
-                raise
-    return (rts, vrfs)
+
+                attract = readvertise.get('attract_traffic', None)
+                if attract:
+                    rts |= set([normalize(rt)
+                                for rt in attract['redirect_rts']])
+
+    return (rts, vpns)
+
+
+def vpn_uid(server, vpn):
+    return "vpn_%s__%s" % (normalize(server), normalize(vpn))
+
+
+def vpn_short(vpn):
+    if len(vpn) > 11:
+        return vpn[:4]+".."+vpn[-5:]
+    else:
+        return vpn
 
 
 def main():
-    usage = """ %prog [options]"""
+    usage = """ %prog [options]
+
+Example: bagpipe-impex2dot --server s1 --server s2 | dot -Tpdf > impex.pdf
+    """
     parser = OptionParser(usage)
 
     parser.add_option(
-        "--server", dest="server", default="127.0.0.1",
-        help="IP address of BaGPipe BGP (optional, default: %default)")
+        "--server", dest="servers", default=[], action="append",
+        help="IP address of a BaGPipe BGP instances (default: 127.0.0.1)")
 
     parser.add_option(
         "--port", dest="port", type="int", default=BAGPIPE_PORT,
@@ -75,63 +112,125 @@ def main():
 
     (options, _) = parser.parse_args()
 
+    if len(options.servers) == 0:
+        options.servers = ["127.0.0.1"]
+
     ports = set()
     dests = set()
     print 'digraph import_export {'
 
-    (rts, vrfs) = get_all(options)
+    print '   node [fontname="Helvetica"];'
+    print '   edge [fontname="Helvetica"];'
+    print '   nodesep=0.55;'
+
+    (rts, vpns) = get_all(options)
     print '   subgraph rts {'
     print '       rank=same;'
     for rt in rts:
         label = rt.upper().replace('_', '\\n')
         print '        %s [shape="circle",label="%s",%s];' % (rt, label,
-                                                              RT_STYLE)
+                                                              RT_TXT_STYLE)
     print '    }'
 
-    print '    subgraph vrfs {'
-    print '       rank=same;'
-    for vrf in vrfs:
-        print '        vrf_%s [label="VRF\\n%s", shape="box"];' % (vrf, vrf)
+    print '    subgraph ipvpns {'
+    print '        rank=same;'
+    for (server, vpn, _) in filter(lambda x: x[2] == 'VRF', vpns):
+        print ('        %s [label="{<0>VRF\\n%s\\n[%s]|<readv>}",'
+               'shape="record"];' % (vpn_uid(server, vpn), vpn_short(vpn),
+                                     server))
     print '    }'
 
-    for vrf_id in vrfs:
+    print '    subgraph evpns {'
+    for (server, vpn, _) in filter(lambda x: x[2] == 'EVI', vpns):
+        print ('        %s [label="EVI\\n%s\\n[%s]",'
+               'shape="box"];' % (vpn_uid(server, vpn), vpn_short(vpn),
+                                  server))
+    print '    }'
 
-        for rt_i in request(options, ["vpns", "instances", vrf_id,
-                                      "route_targets", "import"]):
-            print '        %s -> vrf_%s [%s];' % (normalize(rt_i), vrf_id,
-                                                  RT_STYLE)
-        for rt_e in request(options, ["vpns", "instances", vrf_id,
-                                      "route_targets", "export"]):
-            print '        vrf_%s -> %s [%s];' % (vrf_id, normalize(rt_e),
-                                                  RT_STYLE)
-        try:
-            readvertise = request(options, ["vpns", "instances", vrf_id,
-                                            "readvertise"])
-            if len(readvertise):
-                print ('        %s -> %s [label="via\\n%s",style=dashed,%s];' %
-                       (normalize(readvertise['from'][0]),
-                        normalize(readvertise['to'][0]),
-                        vrf_id,
-                        RT_STYLE))
-        except urllib2.HTTPError as e:
-            if e.code != 404:
-                raise
-        for port in request(options, ["vpns", "instances",
-                                      vrf_id, "ports"]).iterkeys():
-            print ('        vrf_%s -> %s  [style=dashed,dir=none,'
-                   'color="gray",weight=3];' % (vrf_id, normalize(port)))
-            ports.add(normalize(port))
+    for (server, vpn, _) in vpns:
+        print '    /* %s:%s */' % (server, vpn)
+        uid = vpn_uid(server, vpn)
 
-    for port in ports:
-        print '    %s [style=invis,height=0,width=0,fixedsize=true];' % port
+        for rt_i in request(options, server, ["vpns", "instances", vpn,
+                                              "route_targets", "import"]):
+            print '    %s -> %s:0 [%s];' % (normalize(rt_i), uid,
+                                            RT_STYLE)
+
+        for rt_e in request(options, server, ["vpns", "instances", vpn,
+                                              "route_targets", "export"]):
+            print '    %s:0 -> %s [%s];' % (uid, normalize(rt_e),
+                                            RT_STYLE)
+
+        readvertise = request(options, server, ["vpns", "instances",
+                                                vpn, "readvertise"])
+        if readvertise:
+            readv = "%s:readv" % vpn_uid(server, vpn)
+            print ('    %s -> %s [label="",%s];' %
+                   (normalize(readvertise['from'][0]),
+                    readv,
+                    RT_STYLE_REDIR_FROM_RT))
+            print ('    %s -> %s [label="",%s];' %
+                   (readv,
+                    normalize(readvertise['to'][0]),
+                    RT_STYLE_REDIR_TO_RT))
+
+            attract = readvertise.get('attract_traffic', None)
+            if attract:
+                intermediate = "%s_%s_attract" % (normalize(server),
+                                                  normalize(vpn))
+                print ('    %s [style=invis,height=0,width=0,'
+                       'fixedsize=true,rank=1]' % intermediate)
+
+                redir_rt = attract['redirect_rts'][0]
+
+                # readv -> intermediate
+                print ('    %s -> %s [label="flow",%s];' %
+                       (readv, intermediate,
+                        RT_STYLE_FLOWSPEC_INTER))
+
+                # intermediate -> flowspec route RT
+                print ('    %s -> %s [label="",weight=50,%s];' %
+                       (intermediate, normalize(redir_rt),
+                        RT_STYLE_FLOWSPEC))
+
+                # intermediate -> flowspec route redirect action  RT
+                print ('    %s -> %s [label="action",%s,arrowhead=none'
+                       ',weight=20];' %
+                       (intermediate, normalize(readvertise['to'][0]),
+                        RT_STYLE_FLOWSPEC_ACTION))
+
+        for port in request(options, server, ["vpns", "instances",
+                                              vpn, "ports"]).iterkeys():
+            print ('    %s -> port_%s_%s [%s,weight=5];' % (uid,
+                                                            normalize(server),
+                                                            normalize(port),
+                                                            PORT_LINK_STYLE))
+            ports.add((server, normalize(port)))
+
+        # possible link between an E-VPN and an IPVPN ?
+        ipvpn = request(options, server, ["vpns", "instances",
+                                          vpn, "gateway_port", "ipvpn"])
+        if ipvpn:
+            ipvpn_id = ipvpn['external_instance_id']
+            print ('    %s -> %s [weight=500];' % (uid,
+                                                   vpn_uid(server, ipvpn_id))
+                   )
+
+    for (server, port) in ports:
+        print ('    port_%s_%s [label="",style=invis,height=0,width=0,'
+               'fixedsize=true];' % (normalize(server),
+                                     normalize(port)))
         if port.startswith('to_'):
             dest = port.split('_')[1]
-            print ('    %s -> %s [style=dashed,dir=none,color="gray"'
-                   ',weight=3];' %
-                   (port, dest))
-            dests.add(dest)
+            print ('    port_%s_%s -> dest_%s_%s [style=dashed,'
+                   'dir=none,color="gray"'
+                   ',weight=5];' %
+                   (server, normalize(port), normalize(server), dest))
+            dests.add((server, dest))
 
-    for dest in dests:
-        print '    %s [shape="square",color="gray"];' % dest
+    for (server, dest) in dests:
+        print ('    dest_%s_%s [label="%s\\n[%s]",shape="square",'
+               'color="gray"];' % (normalize(server),
+                                   normalize(dest), dest, server))
 
     print '}'
