d9ec59ee8607e986646ce30ded642c44f3298d7d
Thomas Morin <thomas.morin@orange.com>
2016-09-12 14:45:53 +0200

    OVS VPN dataplane: clean error on unresolved nexthop
    Don't attempt to continue when the nexthop of an IP VPN
    route can't be ARP-resolved, raise the error and get a clean log.

--- bagpipe/bgp/vpn/ipvpn/mpls_ovs_dataplane.py
+++ bagpipe/bgp/vpn/ipvpn/mpls_ovs_dataplane.py
@@ -550,6 +550,7 @@ class MPLSOVSVRFDataplane(VPNInstanceDataplane, LookingGlass):
                 except exc.RemotePEMACAddressNotFound as e:
                     self.log.error("An error occured during setupDataplaneFor"
                                    "RemoteEndpoint: %s", e)
+                    raise
 
                 # Map traffic to remote IP address as MPLS on ethX to remote
                 # router MAC address
