Orange-OpenSource/bagpipe-bgp

commit 76323a0caecae259e6542dc92ce3cf29dd8a1bb1

Fix len returned for RTC parsing
len method as returning an (off-by 8) length in bits to the main exabgp loop,
instead of a correct length in bytes, which was resulting in a decoding error
in the case where multiple RTC routes where packed in one BGP update.

Thanks for fxpester for reporting the issue.

bagpipe/exabgp/structure/rtc.py
@@ -53,7 +53,7 @@ class RouteTargetConstraint(object):
         if self.route_target is None:
             return 1
         else:
-            return (4 + len(self.route_target))*8 
+            return (5 + len(self.route_target))
     
     def __str__ (self):
         if self.route_target is None:
@@ -86,7 +86,7 @@ class RouteTargetConstraint(object):
             packedRT = self.route_target.pack()
             # We reset ext com flag bits from the first byte in the packed RT
             # because in an RTC route these flags never appear.
-            return pack("!BL", len(self), self.origin_as) + RouteTargetConstraint.resetFlags(packedRT[0]) + packedRT[1:]
+            return pack("!BL", len(self)*8, self.origin_as) + RouteTargetConstraint.resetFlags(packedRT[0]) + packedRT[1:]
         
     @staticmethod
     def unpack(afi,safi,data):

commit 745d2e932a44358d737175c71c4d682fa190d70f

Fix RTC route encoding
fix issues introduced in 76323a0caecae259e6542dc92ce3cf29dd8a1bb1
(len field count all bytes but itself)

This branch wasn't tested on a platform with a real router :-(

The use_upstream_exabgp branch, relying on cleaner encoding/decoding
routines is much better.

We need to finish the work and make the use_upstream_exabgp branch
the master one, add functional tests there, and make the current master
obsolete.

bagpipe/exabgp/structure/rtc.py
@@ -86,7 +86,7 @@ class RouteTargetConstraint(object):
             packedRT = self.route_target.pack()
             # We reset ext com flag bits from the first byte in the packed RT
             # because in an RTC route these flags never appear.
-            return pack("!BL", len(self)*8, self.origin_as) + RouteTargetConstraint.resetFlags(packedRT[0]) + packedRT[1:]
+            return pack("!BL", (len(self)-1)*8, self.origin_as) + RouteTargetConstraint.resetFlags(packedRT[0]) + packedRT[1:]
         
     @staticmethod
     def unpack(afi,safi,data):